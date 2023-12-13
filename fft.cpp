aa56f86a47a2a32259eaa647f3be5b77b18d58f1
px <pengxiao_email@163.com>
2023-12-13 17:15:30 +0800

    version 1.0.0 支持任意长度的基2FFT操作

--- fft.cpp
+++ fft.cpp
@@ -16,6 +16,10 @@ parameter in_type:
 in_type==0 :in_buf1 以自然位序输入
 in_type==1 :in_buf1 以自然倒位序输入
 
+parameter out_type:
+out_type==0 :out_buf1 以自然位序输出
+out_type==1 :out_buf1 以自然倒位序输出
+
 parameter base:
 base==2 :基2
 base==4 :基4
@@ -40,7 +44,7 @@ int     reverse(int n,int l)                                                    
 void    fft_hcs (int16_t* in_buf,int16_t* out_buf,int16_t fft_length, int8_t base,int8_t in_type);
 void    base2(int16_t i,int16_t i_resp,Complex w,int16_t* in_buf)                                ;
 void    stage_routine(int8_t stage,int16_t length,int8_t base,int16_t* in_buf,int16_t* out_buf)  ;
-
+int8_t  bitwidth(int16_t fft_length);
 //第一种计算旋转因子的方式，直接使用定义计算,计算量较大,计算Wn_k = exp(-2*pi/N*n*k) 其中pi = acos(-1)
 Complex calculate_w(int16_t n,int16_t k,int16_t N){
     //计算Wn_k = exp(-2*pi/N*n*k) 其中pi = acos(-1)
@@ -63,12 +67,38 @@ int reverse(int n,int l){
     }
     return r;
 }
-
-void fft_hcs (int16_t* in_buf,int16_t* out_buf,int16_t fft_length, int8_t base,int8_t in_type){
+int8_t  bitwidth(int16_t fft_length){
+    int8_t bit_width = 0;
+    while(fft_length){
+        fft_length = fft_length>>1;
+        bit_width++;
+    }
+    return bit_width;
+}
+void fft_hcs (int16_t* in_buf,int16_t* out_buf,int16_t fft_length, int8_t base,int8_t in_type,int8_t out_type){
     //计算在此base下需要经过几个stage才能输出最终结果
     int8_t stage_num = logM_n(base,fft_length);
     for(int stage_id = 0; stage_id <stage_num; stage_id++){
         stage_routine(stage_id,fft_length,base,in_buf,out_buf);
+    }
+    // 将in_buf中的数据原封不动/调整位序后输入out_buf
+    for(int i = 0; i<fft_length;i++){
+        switch (out_type)
+        {
+        case 0:
+            //out_type=0,自然位序输出
+            cout<<"outbuf"<<reverse(2*i,bitwidth(fft_length)-1)<<"=inbuf"<<2*i<<endl;
+            out_buf[2*reverse(i,bitwidth(fft_length)-1)]   = in_buf[2*i]  ;
+            out_buf[2*reverse(i,bitwidth(fft_length)-1)+1] = in_buf[2*i+1];
+            break;
+        case 1:
+            out_buf[2*i]   = in_buf[2*i]  ;
+            out_buf[2*i+1] = in_buf[2*i+1];
+            break;
+        default:
+            out_buf[2*i]   = in_buf[2*i]  ;
+            out_buf[2*i+1] = in_buf[2*i+1];
+        }
     }
 }
 
@@ -95,7 +125,7 @@ void stage_routine(int8_t stage,int16_t fft_length,int8_t base,int16_t* in_buf,i
     //解决疑问方法2：用循环来实际测试验证
     //验证结果：
     int16_t data_gap = fft_length/base/pow(2,stage);
-    cout<<" base ="<<fft_length<<" base ="<<base<<" 2^stage = "<<pow(2,stage);
+    cout<<" fft_length ="<<fft_length<<" stage ="<<(int)stage<<" datagap = "<<data_gap<<endl;
     //分block进行运算，先计算存在几个block，每个block的起始地址相隔的距离
     int16_t block_num    = pow(base,stage)     ;
     int16_t block_length = fft_length/block_num;
@@ -108,7 +138,7 @@ void stage_routine(int8_t stage,int16_t fft_length,int8_t base,int16_t* in_buf,i
             switch (base)
             {
                 case 2:
-                    base2(base_addr + cnt,base_addr + cnt + data_gap,calculate_w(1,k,N),in_buf);
+                    base2(base_addr + cnt,base_addr + cnt + data_gap,calculate_w(1,reverse(k,stage),N),in_buf);
                     //找不到旋转因子计算的规律....这一版就写到这,这一句有问题
                     break;
                 default:
@@ -124,9 +154,11 @@ int main(){
     ifstream  infile("fft_data/fft_oai_in1.txt",ios::in);
     int16_t in_data[100000];
     int16_t out_result[100000];
-    int fft_length=0;
-    while(infile)infile>>in_data[fft_length++];
-    fft_length-=1;
+    int data_length=0;
+    while(infile)infile>>in_data[data_length++];
+    data_length-=1;
+    //一半是实部一半是虚部
+    int fft_length = data_length/2;
     #ifdef print_in_out  
     cout<<"len = "<<fft_length<<endl;
     cout<<"原始数据："<<endl;
@@ -134,11 +166,21 @@ int main(){
     #endif
     cout<<logM_n(8,4096);
     cout<<endl<<"---------------------------"<<endl;
-    //-----------------------------------------------
-    int8_t base    = 2;
-    int8_t in_type = 1;
-    fft_hcs(in_data,out_result,fft_length,base,in_type);
+    //--------------准备运行参数，进行FFT---------------------------
+    int8_t base     = 2;
+    int8_t in_type  = 0;
+    int8_t out_type = 0;
+    fft_hcs(in_data,out_result,fft_length,base,in_type,out_type);
     #ifdef print_in_out  
     for(int i=0;i<fft_length;i++)cout<<in_data[i]<<' ';
     #endif
+    //-------------将结果写入文件-----------------------------------
+    std::ofstream outfile("fft_data/fft_oai_out_1_cpp.txt", std::ios::out);
+    for (int i = 0; i < data_length; i++) {
+        outfile << out_result[i] << endl;
+    }
+    cout<<(int)bitwidth(2)<<endl;
+    cout<<(int)bitwidth(2048)<<endl;
+    cout<<(int)bitwidth(1024)<<endl;
+    // outfile.close(); 
 } 
